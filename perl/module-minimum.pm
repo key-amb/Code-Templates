6105315398b45028419b95b909bca3c57cbf5a08
key-amb <yasutake.kiyoshi@gmail.com>
2015-05-12 23:30:42 +0900

    Update year

--- perl/module-minimum.pm
+++ perl/module-minimum.pm
@@ -56,7 +56,7 @@ YASUTAKE Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
 
 =head1 LICENSE
 
-Copyright (C) 2014 YASUTAKE Kiyoshi.
+Copyright (C) 2015 YASUTAKE Kiyoshi.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.  That means either (a) the GNU General Public
