5285a7439738215fc4165f69775ffba21686cb97
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-25 12:28:58 +0900

    Add minimum perl script

---
+++ perl/script-minimum.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use 5.010_001;
+
+#say 'foo';
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+B<program_name> - abstract
+
+=head1 SYNOPSIS
+
+    program_name ...
+
+=head1 DESCRIPTION
+
+This script is ...
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHORS
+
+YASUTAKE Kiyoshi E<lt>yasutake.kiyoshi@gmail.comE<gt>
+
+=head1 LICENSE
+
+Copyright (C) 2015 YASUTAKE Kiyoshi.
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.  That means either (a) the GNU General Public
+License or (b) the Artistic License.
+
+=cut
+
